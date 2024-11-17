310af5a0f028a7547a0b458aa53de3555ee8a9f9
ynnarettere <denispak2005@gmail.com>
2024-11-17 21:58:08 +0500

    Update User.js

--- 2/сейв/models/User.js
+++ 2/сейв/models/User.js
@@ -1,20 +1,12 @@
 const mongoose = require('mongoose');
 
-const userSchema = mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  role: {
-    type: String,
-    enum: ['user', 'admin'],
-    default: 'user',
-  },
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: String, required: true },
 });
 
-module.exports = mongoose.model('User', userSchema);
+// Указываем коллекцию 'users'
+const User = mongoose.model('User', userSchema, 'users');
+
+module.exports = User;
